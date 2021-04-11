7a183a9601d5d5b5fe08a99011f643289cc087bf
cheekezie <harrychikezie1@gmail.com>
2021-04-12 00:18:59 +0100

    new website template implemented

--- js/site.js
+++ js/site.js
@@ -11,20 +11,23 @@ $(document).ready(function(){
 		customPaging : (slider, i) => {
 			return '<a>'+(i+1)+'</a>';
 			},
-	  });
+	});
 
-	  // EXPAND FORM
-	  $(".js-show-form").click(() => {
-		$(".expanding-form").toggleClass("hide");
+	// Alternative slider
+	$('.features__slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
 	  });
+	  
+	// SIDE MENU TOGGLE
+	$(".menu-toggle").click(() => {
+	$(".mobile-menu").toggleClass("motion-in");
+	});
 
-	  // SIDE MENU TOGGLE
-	  $(".menu-toggle").click(() => {
-		$(".mobile-menu").toggleClass("motion-in");
-	  });
-
-	  //LOADER EFFECT
-	  $(".loader").delay(200).fadeOut("slow");
+	//LOADER EFFECT
+	$(".loader").delay(200).fadeOut("slow");
 });
 
 AOS.init({
